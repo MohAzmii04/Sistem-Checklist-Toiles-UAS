632be5bec4b50b21cb808414173fcd5f0cdea0c4
iki020904 <muhammadrifqiaziz15@gmail.com>
2024-01-09 22:41:24 +0700

    file baru

--- UAS/ind_toilet.php
+++ UAS/ind_toilet.php
@@ -17,7 +17,7 @@ $result = mysqli_query($conn, $sql);
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Hidayat Tulloh UAS</title>
+    <title>UAS Sistem Toilet Checklist</title>
     <link href="style.css" rel="stylesheet" type="text/css" />
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
 
